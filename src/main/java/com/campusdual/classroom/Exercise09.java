02e539d25ca700d32c1d65f0b30e6c429d0d1952
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 11:53:12 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(15);
     }
 
     //TODO ↓
@@ -12,6 +12,10 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int n1 = 0;
+        do {
+            System.out.println("Entra con valor: " + n1 + ", sale con valor: " + (n1+1));
+            n1++;
+        } while (n1 < num);
     }
 }
